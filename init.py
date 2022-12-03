7d5b180f3360d715e2c5fd685297dba2dbc834ba
Viktoro Urso <46228128+ViktoroUrso@users.noreply.github.com>
2022-12-03 21:19:46 +0300

    Create init.py

---
+++ init.py
@@ -0,0 +1,5 @@
+# Copyright (C) 2020-20022 Viktoro Urso
+# Author: Victor Medved
+# Contact: viktoro.urso@gmail.com
+
+"""A package for creating, manipulating and presenting geometric objects on the 2D plane.""" 
